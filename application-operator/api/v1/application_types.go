9bbafd50096afa88d528dd14de1fa70d038aaa21
rui <771963601@qq.com>
2026-01-31 17:00:31 +0800

    Implement Reconcile()

--- application-operator/api/v1/application_types.go
+++ application-operator/api/v1/application_types.go
@@ -17,8 +17,8 @@ limitations under the License.
 package v1
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
@@ -34,8 +34,8 @@ type ApplicationSpec struct {
 	// foo is an example field of Application. Edit application_types.go to remove/update
 	// +optional
 	// Foo *string `json:"foo,omitempty"`
-	Replicas int32                   `json:"replicas,omitempty"`  
-	Template corev1.PodTemplateSpec  `json:"template,omitempty"`  
+	Replicas int32                  `json:"replicas,omitempty"`
+	Template corev1.PodTemplateSpec `json:"template,omitempty"`
 }
 
 // ApplicationStatus defines the observed state of Application.
--- application-operator/internal/controller/application_controller.go
+++ application-operator/internal/controller/application_controller.go
@@ -18,13 +18,18 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
-
-	appsv1 "jack.cn/application-operator/api/v1"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	// appsv1 "jack.cn/application-operator/api/v1"
+	jappsv1 "jack.cn/application-operator/api/v1"
 )
 
 // ApplicationReconciler reconciles a Application object
@@ -47,17 +52,45 @@ type ApplicationReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.23.1/pkg/reconcile
 func (r *ApplicationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = logf.FromContext(ctx)
+	l := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	// get the Application
+	app := &jappsv1.Application{}
+	if err := r.Get(ctx, req.NamespacedName, app); err != nil {
+		if errors.IsNotFound(err) {
+			l.Info("the Application is not found")
+			return ctrl.Result{}, nil
+		}
+		l.Error(err, "failed to get the Application")
+		return ctrl.Result{RequeueAfter: 1 * time.Minute}, err
+	}
 
+	// create pods
+	for i := 0; i < int(app.Spec.Replicas); i++ {
+		pod := &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      fmt.Sprintf("%s-%d", app.Name, i),
+				Namespace: app.Namespace,
+				Labels:    app.Labels,
+			},
+			Spec: app.Spec.Template.Spec,
+		}
+
+		if err := r.Create(ctx, pod); err != nil {
+			l.Error(err, "failed to create Pod")
+			return ctrl.Result{RequeueAfter: 1 * time.Minute}, err
+		}
+		l.Info(fmt.Sprintf("the Pod (%s) has created", pod.Name))
+	}
+
+	l.Info("all pods has created")
 	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *ApplicationReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&appsv1.Application{}).
+		For(&jappsv1.Application{}).
 		Named("application").
 		Complete(r)
 }
